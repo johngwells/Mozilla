b074966bd9496f5b456cf835e09363571a0642de
John Wells <molekewl2017@gmail.com>
2019-07-07 00:05:35 -0700

    study material

--- indexedCollections.js
+++ indexedCollections.js
@@ -39,3 +39,64 @@ let misuArray = Array.of(9.3);
 console.log(misuArray);
 
 // Populating an array
+
+// you can populate an array by assigning values to it elements
+var emp = [];
+emp[0] = 'Casey Jones';
+emp[1] = 'Phil Lesh';
+emp[2] = 'August West';
+
+var arr = [];
+arr[3.4] = 'oranges';
+console.log(arr);
+// check for property
+console.log(arr.hasOwnProperty(3.4));
+// check value
+console.log(Object.values(arr).includes('Oranges'));
+
+// You can populate an array when you create it
+var myArray = new Array('Hello', myVar, 3.14159);
+var myArray = ['Mange', "Apple", 'Orange'];
+
+// *** Referring To Array Elements ***
+
+// You refer to an array's elements by using the elements orginal number
+var myArray = ['Wind', 'Rain', 'Fire'];
+myArray[0];
+myArray[1];
+
+// the array operator(square brackets) is also used to access the array's properties
+// arrays are also objects
+myArray['length']; // 3
+
+// *** Understanding Length ***
+// JS arrays actually store their elements as standard object properties
+// using the array index as the property name
+var cats = [];
+cats[30] = ['Dusty'];
+console.log(cats.length); // 31
+
+// You can also assign to the length property. Writing 0 empties it entirely
+var cats = ['Dusty', 'Misty', 'Twiggy'];
+console.log(cats.length); // 3
+
+cats.length = 2; // removes Twiggy
+cats.length = 0 // cats array is empty
+cats.length = 3 // logs [ <3 empty items> ]
+
+// *** Iterating over arrays ***
+// A common operation is to iterate over the values of an array
+// processing each one in some way
+
+var colors = ['red', 'green', 'blue'];
+for (var i = 0; i < colors.length; i++) {
+  console.log(colors[i]);
+}
+
+// foreach() method provides another way of iterating
+var colors = ['red', 'green', 'blue'];
+colors.forEach(function(color) {
+  console.log(color);
+});
+// you can shorten with arrow functions
+colors.forEach(color => console.log(color));
